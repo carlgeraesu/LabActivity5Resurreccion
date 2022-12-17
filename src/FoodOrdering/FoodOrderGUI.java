c1dcda4332a6caa2bf90a5bd4790163ec5ca4cf9
carlgeraesu <resurreccioncarlgerard@gmail.com>
2022-12-17 17:03:29 +0800

    Add exception handling to FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -6,6 +6,12 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
+
+class Nochoices extends Exception {
+    public Nochoices(String s) {
+        super(s);
+    }
+}
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
     private JCheckBox cPizza;
@@ -30,35 +36,60 @@ public class FoodOrderGUI extends JFrame {
         btnOrder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+            try{
                     float res = 0;
+                    int y = 0;
 
                     if (cPizza.isSelected() == true) {
                         res += 100;
+                        y++;
                     }  if (cSoftDrinks.isSelected() == true) {
                         res += 55;
+                        y++;
                     }  if (cFries.isSelected() == true) {
                         res += 65;
+                        y++;
                     }  if (cBurger.isSelected() == true) {
                         res += 80;
+                        y++;
                     }  if (cTea.isSelected() == true) {
                         res += 50;
+                        y++;
                     }  if (cSundae.isSelected() == true) {
                         res += 40;
+                        y++;
                     }
+
+                    int z = 0;
 
                     if(rb5.isSelected() == true){
                         res *= 0.95;
+                        z++;
                         JOptionPane.showMessageDialog(btnOrder,"The total price is Php " + String.format("%.2f", res));
                     } else if (rb10.isSelected() == true) {
                         res *= 0.90;
+                        z++;
                         JOptionPane.showMessageDialog(btnOrder, "The total price is Php " + String.format("%.2f",res));
                     } else if (rb15.isSelected() == true) {
                         res *= 0.85;
+                        z++;
                         JOptionPane.showMessageDialog(btnOrder, "The total price is Php " + String.format("%.2f",res));
                     } else if (rbNone.isSelected() == true) {
+                        z++;
                         JOptionPane.showMessageDialog(btnOrder, "The total price is Php " + String.format("%.2f",res));
                     }
+
+                if(y == 0 && z == 1){
+                    throw (new Nochoices("Please pick an order/discount."));
+                } else if(y > 0 && z == 0){
+                    throw (new Nochoices("Please pick a discount."));
+                } else if (y == 0 && z == 0) {
+                    throw (new Nochoices("Please choose from the choices."));
+                }
+
+            } catch (Exception c){
+                JOptionPane.showMessageDialog(null, c.getMessage());
+            }
             }
         });
     }
