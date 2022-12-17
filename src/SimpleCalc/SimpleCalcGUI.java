2167a0b2f36b6f0fe41c256be0a27847c9a647bb
carlgeraesu <resurreccioncarlgerard@gmail.com>
2022-12-17 14:56:35 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,9 @@
 package SimpleCalc;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,7 +15,45 @@ public class SimpleCalcGUI extends JFrame{
     private JButton btnCompute;
     private JTextField lblResult;
 
-    public static void main(String[] args) {
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                    int n1 = Integer.parseInt(tfNumber1.getText());
+                    int n2 = Integer.parseInt(tfNumber2.getText());
+                    int res;
+
+                    if (cbOperations.getSelectedItem().equals('+')) {
+                       res = n1 + n2;
+                       lblResult.setText(String.valueOf(res));
+                    } else if (cbOperations.getSelectedItem().equals('-')) {
+                        res = n1 - n2;
+                        lblResult.setText(String.valueOf(res));
+                    } else if (cbOperations.getSelectedItem().equals('*')) {
+                        res = n1 * n2;
+                        lblResult.setText(String.valueOf(res));
+                    }  else if (cbOperations.getSelectedItem().equals('/')){
+                        res = n1 / n2;
+                        lblResult.setText(String.valueOf(res));
+                    }
+
+            }
+        });
     }
+
+    public static void main(String[] args) {
+        SimpleCalcGUI clc = new SimpleCalcGUI();
+        clc.cbOperations.addItem('+');
+        clc.cbOperations.addItem('-');
+        clc.cbOperations.addItem('*');
+        clc.cbOperations.addItem('/');
+        clc.setTitle("Simple Calculator");
+        clc.setContentPane(clc.panel1);
+        clc.setSize(700,700);
+        clc.setVisible(true);
+        clc.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
 }
